12268bd265dcf6feae2a3b19bb4a6ff049ecc2d0
vijaykumarvj97 <emmanuelvijaykumar007@gmail.com>
2022-09-08 18:21:39 +0530

    upload DEX, DAO

---
+++ DAO-Tutorial/hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x339EC62A542a6aEbfC66847ad9613e4A41c3F5e3";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
